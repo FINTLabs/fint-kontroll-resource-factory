2a78aa2a5133cbd482f44dc7d89fedd83cfacebd
berntjoh <berntj@viken.no>
2024-12-03 13:30:02 +0100

    KFS-1071 map license model to enforcement category

--- src/main/java/no/fintlabs/applicationResource/ApplicationResourceService.java
+++ src/main/java/no/fintlabs/applicationResource/ApplicationResourceService.java
@@ -1,12 +1,15 @@
 package no.fintlabs.applicationResource;
 
+import no.fint.model.resource.Link;
 import no.fintlabs.applicationResourceLocation.ApplicationResourceLocationService;
 import no.fintlabs.fintResourceModels.resource.eiendeler.applikasjon.LisensResource;
 import no.fintlabs.fintResourceServices.*;
 import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.cache.FintCache;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +25,17 @@ public class ApplicationResourceService {
     private final FintResourceApplikasjonsKategoriService fintResourceApplikasjonsKategoriService;
     private final ApplicationResourceLocationService applicationResourceLocationService;
     private final ApplicationResourceEntityProducerService applicationResourceEntityProducerService;
+
+    @Value("${fint.kontroll.resource.license-enforcement.hard-stop}")
+    private List<String> hardStopLicenseModels;
+    @Value("${fint.kontroll.resource.license-enforcement.floating}")
+    private List<String> floatingLicenseModels;
+    @Value("${fint.kontroll.resource.license-enforcement.free-all}")
+    private List<String> freeAllLicenseModels;
+    @Value("${fint.kontroll.resource.license-enforcement.free-edu}")
+    private List<String> freeEduLicenseModels;
+    @Value("${fint.kontroll.resource.license-enforcement.free-student}")
+    private List<String> freeStudentLicenseModels;
 
     public ApplicationResourceService(FintCache<String, LisensResource> lisensResourceFintCache,
                                       FintResourceLisensService fintResourceLisensService,
@@ -72,15 +86,45 @@ public class ApplicationResourceService {
         applicationResource.setResourceOwnerOrgUnitId(fintResourceLisensService.getResourceOwnerOrgUnitId(lisensResource));
         applicationResource.setResourceOwnerOrgUnitName(fintResourceLisensService.getResourceOwnerOrgUnitName(lisensResource));
         applicationResource.setResourceLimit(fintResourceLisensService.getResourceLimit(lisensResource));
-        applicationResource.setPlatform(fintResourcePlattformService.getPlatform(lisensResource));
+        //applicationResource.setPlatform(fintResourcePlattformService.getPlatform(lisensResource));
         applicationResource.setAccessType(fintResourceLisensmodelService.getAccessType(lisensResource));
         applicationResource.setValidForRoles(fintResourceBrukertypeService.getValidForRoles(lisensResource));
         applicationResource.setValidForOrgUnits(applicationResourceLocationService.getValidForOrgunits(lisensResource));
         applicationResource.setApplicationCategory(fintResourceApplikasjonsKategoriService.getApplikasjonskategori(lisensResource));
         // Nye felter 3.18
-//        applicationResource.setLicenseEnforcement();
+        //applicationResource.setLicenseModel(fintResourceLisensmodelService.getLicenseModel(lisensResource));
+        applicationResource.setLicenseEnforcement(mapLicenseModelToLicenseEnforcement(lisensResource));
         applicationResource.setStatus("ACTIVE");
 //        applicationResource.setStatusChanged();
         return Optional.of(applicationResource);
+    }
+    private String mapLicenseModelToLicenseEnforcement(LisensResource lisensResource) {
+        Optional<String> licenseModel =
+                lisensResource.getLisensmodell()
+                        .stream()
+                        .findFirst()
+                        .map(Link::getHref);
+
+        if (licenseModel.isEmpty()) {
+            return "NOT-SPECIFIED";
+        }
+        String licenseModelId = StringUtils.substringAfterLast(licenseModel.get(), "/");
+
+        if (hardStopLicenseModels.contains(licenseModelId)) {
+            return "HARD-STOP";
+        }
+        if (floatingLicenseModels.contains(licenseModelId)) {
+            return "FLOATING";
+        }
+        if (freeStudentLicenseModels.contains(licenseModelId)) {
+            return "FREE-STUDENT";
+        }
+        if (freeEduLicenseModels.contains(licenseModelId)) {
+            return "FREE-EDU";
+        }
+        if (freeAllLicenseModels.contains(licenseModelId)) {
+            return "FREE-ALL";
+        }
+        return "FREE-ALL";
     }
 }
