72194be92a3b8f3b021b35664713b29e68638cff
berntjoh <berntj@viken.no>
2025-01-22 13:32:54 +0100

    FKS-1073  add logging of license model mapping

--- src/main/java/no/fintlabs/applicationResource/LicenseModelMapping.java
+++ src/main/java/no/fintlabs/applicationResource/LicenseModelMapping.java
@@ -1,5 +1,6 @@
 package no.fintlabs.applicationResource;
 
+import lombok.extern.slf4j.Slf4j;
 import no.fint.model.resource.Link;
 import no.fintlabs.fintResourceModels.resource.eiendeler.applikasjon.LisensResource;
 import no.fintlabs.kodeverk.Handhevingstype;
@@ -7,12 +8,17 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.util.Optional;
 
+@Slf4j
 public class LicenseModelMapping {
 
     public static String mapLicenseModelToLicenseEnforcement(
             LisensResource lisensResource,
             ApplicationResourceConfiguration applicationResourceConfiguration
     ){
+        log.info("Mapping license model to license enforcement for lisensResource ({}): {}",
+                lisensResource.getSystemId().getIdentifikatorverdi(),
+                lisensResource.getLisensnavn()
+        );
         Optional<String> licenseModel =
                 lisensResource.getLisenstilgang()
                         .stream()
@@ -20,25 +26,36 @@ public class LicenseModelMapping {
                         .map(Link::getHref);
 
         if (licenseModel.isEmpty()) {
+            log.warn("No license model found for lisensResource with systemId: {}",
+                    lisensResource.getSystemId().getIdentifikatorverdi()
+            );
             return Handhevingstype.NOTSPECIFIED.name();
         }
         String licenseModelId = StringUtils.substringAfterLast(licenseModel.get(), "/");
 
         if (applicationResourceConfiguration.getLicenseEnforcement().getHardStop().contains(licenseModelId)) {
+            log.info("License model {} mapped to HARDSTOP", licenseModelId);
             return Handhevingstype.HARDSTOP.name();
         }
         if (applicationResourceConfiguration.getLicenseEnforcement().getFloating().contains(licenseModelId)) {
+            log.info("License model {} mapped to FLOATING", licenseModelId);
             return Handhevingstype.FLOATING.name();
         }
         if (applicationResourceConfiguration.getLicenseEnforcement().getFreeStudent().contains(licenseModelId)) {
+            log.info("License model {} mapped to FREESTUDENT", licenseModelId);
             return Handhevingstype.FREESTUDENT.name();
         }
         if (applicationResourceConfiguration.getLicenseEnforcement().getFreeEdu().contains(licenseModelId)) {
+            log.info("License model {} mapped to FREEEDU", licenseModelId);
             return Handhevingstype.FREEEDU.name();
         }
         if (applicationResourceConfiguration.getLicenseEnforcement().getFreeAll() .contains(licenseModelId)) {
+            log.info("License model {} mapped to FREEALL", licenseModelId);
             return Handhevingstype.FREEALL.name();
         }
+        log.warn("No license enforcement found for license model {}. license enforcement set to FREEALL",
+                licenseModelId
+        );
         return Handhevingstype.FREEALL.name();
     }
 }
